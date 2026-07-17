69bea80904e165a51c8665e12f1b54e4b63c58f4
banzaman <markrubanza10@gmail.com>
2026-07-17 13:57:50 -0700

    fix: update robots host and sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -10,7 +10,7 @@ export default function robots(): MetadataRoute.Robots {
         disallow:  ["/api/", "/_next/"],
       },
     ],
-    sitemap: `${SITE.url}/sitemap.xml`,
-    host:    SITE.url,
+    sitemap: `https://surrayaislamicschool.com/sitemap.xml`,
+    host:    "https://surrayaislamicschool.com",
   };
 }
